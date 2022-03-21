50a1b530cedd957616b0e1f398bc49e910441d87
labadcloyd <abadcloyd@gmail.com>
2022-03-21 16:26:16 +0800

    added conversions for variables

--- variables.go
+++ variables.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var (
 	firstName = "cloyd"
@@ -32,4 +35,21 @@ func main() {
 
 	j = 40.0
 	fmt.Println(j)
+
+	//converting types
+	var floatingNumber float32 = 25.5
+	var intNumber = 10
+	intNumber = int(floatingNumber)
+
+	fmt.Printf("%v, %T", intNumber, intNumber)
+
+	//converting string
+	var age = 20
+	var ageString = "20"
+	var stringAge = strconv.Itoa(age)
+	fmt.Printf("\n %v, %T", stringAge, stringAge)
+	
+	newAge, err:= strconv.Atoi(ageString)
+	fmt.Printf("\n %v, %T error: %v", newAge, newAge, err)
+
 }
