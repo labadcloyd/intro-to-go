089a2705d01174ad9780c3fe2e8f4b0a3cacc2a3
labadcloyd <abadcloyd@gmail.com>
2022-03-24 19:32:29 +0800

    added more details to arraysAndSlices

--- src/arraysAndSlices.go
+++ src/arraysAndSlices.go
@@ -8,16 +8,22 @@ func main() {
 	*/
 	grades := [3]int{87, 89, 88}
 	fmt.Printf("\n%v, %T", grades, grades)
+	fmt.Printf("\n")
+
 
 	// you dont have to specify the size when initiating an array
 	grades2 := [...]int{87, 89, 88}
 	fmt.Printf("\n%v, %T", grades2, grades2)
+	fmt.Printf("\n")
+
 
 	// another way to declare arrays
 	var students [4]string
 	students[0] = "Cloyd"
 	fmt.Printf("\nStudents: %v, %T", students, students)
 	fmt.Printf("\nLength of array of students: %v, %T", len(students), students)
+	fmt.Printf("\n")
+
 
 	// you can also declare multi dimensional arrays
 	// using identity matrix as an example, this is related to linear algebra
@@ -26,6 +32,8 @@ func main() {
 	identityMatrix[1] = [3]int { 0, 1, 0 }
 	identityMatrix[2] = [3]int { 0, 0, 1 }
 	fmt.Printf("\n%v, %T", identityMatrix, identityMatrix)
+	fmt.Printf("\n")
+
 
 	// when you try to copy an array you make an entirely new array
 	// which apparently other languages don't function like that
@@ -35,13 +43,17 @@ func main() {
 	b[1] = 26
 	fmt.Printf("\n%v", a)
 	fmt.Printf("\n%v", b)
+	fmt.Printf("\n")
 
-	f := a[2:] // array after 2nd element to end
-	g := a[:4] // first 4 elements
+	f := a[2:] // elements skipped from the start
+	g := a[:3] // first 3 elements
 	h := a[1:4] // 2nd element to 4th element
-	fmt.Printf("\nArray after 2nd element to end %v", f)
-	fmt.Printf("\nFirst 4 elements: %v", g)
+	i := a[:] // all elements
+	fmt.Printf("\nElements skipped from the start: %v", f)
+	fmt.Printf("\nFirst 3 elements: %v", g)
 	fmt.Printf("\n2nd element to 4th element: %v", h)
+	fmt.Printf("\nAll Elements: %v", i)
+	fmt.Printf("\n")
 
 	/*
 	* SLICES
@@ -51,6 +63,7 @@ func main() {
 	// ing the three dots which we use with arrays
 	c := []int {99, 98, 97}
 	fmt.Printf("\nSlices: %v", c)
+	fmt.Printf("\n")
 
 	// slices are naturally reference types
 	// so when you copy an array to a variable, youre not creating a new array
@@ -59,5 +72,50 @@ func main() {
 	d[1] = 90
 	fmt.Printf("\nOriginal Slices: %v", c)
 	fmt.Printf("\nReference Slices: %v", d)
+	fmt.Printf("\n")
+	
+	// when creating slices you can increase the capacity by appending data
+	a2 := []int{}
+	fmt.Printf("\nLength: %v, Capacity: %v", len(a2), cap(a2))
+	fmt.Printf("\nvalue: %v ", a2)
+	a2 = append(a2, 3, 4, 5, 6)
+	fmt.Printf("\nLength: %v, Capacity: %v", len(a2), cap(a2))
+	fmt.Printf("\nvalue: %v ", a2)
+	fmt.Printf("\n")
+	// however using this way can be expensive when dealing with larger slices
+	// an alternative to this would by using the make function on declaring the var
+	// because this way the compiler can know ahead of time the capacity of the array
+	a3 := make([]int, 0, 100) 
+	fmt.Printf("\nDeclaring slices using the make function")
+	fmt.Printf("\nLength: %v, Capacity: %v", len(a3), cap(a3))
+	fmt.Printf("\nvalue: %v ", a3)
+	a3 = append(a3, 3, 4, 5, 6)
+	fmt.Printf("\nLength: %v, Capacity: %v", len(a3), cap(a3))
+	fmt.Printf("\nvalue: %v ", a3)
+	fmt.Printf("\n")
+	
+	// go also has the equivalent of javascript's spread operator
+	a3 = append(a3, []int{ 3, 4, 5, 6 }...)
+	fmt.Printf("\nUsing the spread operator")
+	fmt.Printf("\nLength: %v, Capacity: %v", len(a3), cap(a3))
+	fmt.Printf("\nvalue: %v ", a3)
+	fmt.Printf("\n")
+	
+	// how to pop elements off a slice
+	b2 := a3[1 : len(a3)-1]
+	fmt.Printf("\nPopping off elements from a slice")
+	fmt.Printf("\nLength: %v, Capacity: %v", len(b2), cap(b2))
+	fmt.Printf("\nvalue: %v ", b2)
+	fmt.Printf("\n")
+
+	// popping off an element in the middle is tricky
+	// but you can pull it off by appending
+	// first param is taking the first 3 elements
+	// second param is the elements after the first 5 elements
+	b2 = append(a3[:3], a3[5:]...)
+	fmt.Printf("\nPopping off middle elements from a slice")
+	fmt.Printf("\nLength: %v, Capacity: %v", len(b2), cap(b2))
+	fmt.Printf("\nvalue: %v ", b2)
+	fmt.Printf("\n")
 
 }
