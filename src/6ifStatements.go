16fc8a91583cb41efdb87cfbec6f9960e91cf347
labadcloyd <abadcloyd@gmail.com>
2022-04-01 14:03:29 +0800

    added if statements

--- src/6ifStatements.go
+++ src/6ifStatements.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	/*
+	* IF STATEMENTS
+	*/
 	if true {
 		fmt.Printf("True")
 		fmt.Printf("\n")
@@ -18,6 +21,45 @@ func main() {
 	}
 
 	if _, floridaExists := statePopulation["Florida"]; floridaExists {
-		fmt.Printf("Florida Exists")
+		fmt.Printf("Florida Exists\n")
 	}
+
+	// basic conditional operators
+	num1 := 30
+	num2 := 40
+
+	fmt.Println(num1 <= num2, num1 >= num2, num1 != num2)
+	fmt.Println(num1 <= num2 || num1 >= num2)
+	fmt.Println(num1 <= num2 && num1 >= num2)
+	fmt.Println(num1 <= num2 || !(num1 >= num2))
+	fmt.Println(num1 <= num2 && !(num1 >= num2))
+
+	// when writing conditionals with logical operators
+	// go checks it from left to right, and in an or operator
+	// if one is true then go will no longer check the remaining
+	// statements
+	fmt.Printf("\n")
+	returnTrue := func() bool {
+		// functions cannot be declared inside a function unless
+		// you use variables and assign a function to it
+		fmt.Println("Returned True")
+		return true
+	}
+
+	if num1 < 1 || returnTrue() || num1 > 1 {
+		fmt.Println("function return true was called")
+	}
+	if num1 > 1 || returnTrue() || num1 > 1 {
+		fmt.Println("function return true was NOT called")
+		fmt.Printf("\n")
+	}
+	if num1 > 1 && returnTrue() {
+		fmt.Println("function return true was called")
+	}
+	if num1 < 1 && returnTrue() {
+		fmt.Println("function return true was NOT called")
+	}
+
+
+
 }
